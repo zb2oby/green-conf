7e84cff1d7537569eaa745b014a2eac3baad8bf1
Mom0tte777 <120727062+Mom0tte777@users.noreply.github.com>
2023-01-13 09:12:51 +0100

    gestion optimisme

--- src/main/java/fr/fortil/irma/service/impl/TirageServiceImpl.java
+++ src/main/java/fr/fortil/irma/service/impl/TirageServiceImpl.java
@@ -12,13 +12,17 @@ import fr.fortil.irma.dto.Tirage;
 import fr.fortil.irma.dto.TirageComplet;
 import fr.fortil.irma.dto.Tirages;
 import fr.fortil.irma.service.ICarteService;
+import fr.fortil.irma.service.IStockageService;
 import fr.fortil.irma.service.ITirageService;
 
 @Service
 public class TirageServiceImpl implements ITirageService {
-	
+
 	@Autowired
 	private ICarteService carteService;
+
+	@Autowired
+	private IStockageService stockageService;
 
 	@Override
 	public InitTirage initierTirage(Joueur joueur) {
@@ -28,10 +32,16 @@ public class TirageServiceImpl implements ITirageService {
 	}
 
 	@Override
-	public Tirage getTirageUnique(String numTirage) {
+	public Tirage getTirageUnique(UUID numTirage, Integer positif) {
 		Tirage tirage = new Tirage();
-		tirage.setNumTirage(numTirage);
-		tirage.setCarte(carteService.piocherCarte());
+		tirage.setNumTirage(numTirage.toString());
+		tirage.setCarte(carteService.piocherCarte(positif));
+		if(stockageService.isTirageComplet(numTirage)) {
+			return null;
+		}
+		if (!stockageService.checkCarteDejaTiree(numTirage, tirage.getCarte())) {
+			this.getTirageUnique(numTirage, positif);
+		}
 		return tirage;
 	}
 
@@ -46,12 +56,14 @@ public class TirageServiceImpl implements ITirageService {
 
 	private Tirages initierTirageComplet(String numTirage) {
 		Tirages tirages = new Tirages();
-		tirages.add(this.getTirageUnique(numTirage));
+		for (int i = 0; i < 6; i++) {
+			tirages.add(this.getTirageUnique(UUID.fromString(numTirage), 2));
+		}
 		return tirages;
 	}
 
 	@Override
-	public Conclusion getConclusion(String numTirage) {
+	public Conclusion getConclusion(UUID numTirage) {
 		// TODO Auto-generated method stub
 		return new Conclusion();
 	}
